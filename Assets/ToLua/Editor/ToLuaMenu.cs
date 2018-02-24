0f969a0f4e0fbcf5bfd9a0860d873d15016abfe8
chenxm <chxm.mail@gmail.com>
2018-02-24 21:50:43 +0800

    fix Source Generator

--- Assets/ToLua/Editor/ToLuaMenu.cs
+++ Assets/ToLua/Editor/ToLuaMenu.cs
@@ -106,9 +106,13 @@ public static class ToLuaMenu
     static ToLuaMenu()
     {
         string dir = CustomSettings.saveDir;
+        if(!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
         string[] files = Directory.GetFiles(dir, "*.cs", SearchOption.TopDirectoryOnly);
 
-        if (files.Length < 3 && beCheck)
+        if (files == null || files.Length < 3 && beCheck)
         {
             if (EditorUtility.DisplayDialog("自动生成", "点击确定自动生成常用类型注册文件， 也可通过菜单逐步完成此功能", "确定", "取消"))
             {
